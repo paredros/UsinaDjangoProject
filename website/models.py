cb11ce57d4b6475744359beab624354a311a269e
paredros <paredros@gmail.com>
2025-03-12 20:02:05 -0300

    agregando el modelo de proyecto

--- website/models.py
+++ website/models.py
@@ -10,3 +10,12 @@ class Scrollytelling(models.Model):
 
     def __str__(self):
         return self.name_key
+
+class Proyecto(models.Model):
+    titulo = models.CharField(max_length=120)
+    slide_text = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=200)
+    slug = models.CharField(max_length=120, unique=True)
+    copete = models.TextField()
+    texto = models.TextField()
+
